825a37484287e11124b3e5eeeec5519dea9e1f00
adityarn <aditya_reddy.nagaram@alcatel-lucent.com>
2018-07-20 10:24:48 +0200

    Validate subnet gateway clear not allowed when attached to a router.
    Change-Id: Ib53f0e32b97458521086ae04e38a5c7d0910c56c
    Closes-Bug: OPENSTACK-2240

--- nuage_tempest_plugin/tests/api/test_subnets.py
+++ nuage_tempest_plugin/tests/api/test_subnets.py
@@ -43,3 +43,24 @@ class SubnetsTest(NuageAdminNetworksTest):
             subnet2 = self.create_subnet(network, cidr=IPNetwork(
                 "20.0.0.0/24"), mask_bits=28)
             self.assertIsNotNone(subnet2, "Unable to create second subnet")
+
+    def test_router_attached_subnet_update_clear_gateway_negative(self):
+        network = self.create_network()
+        subnet = self.create_subnet(network)
+        router = self.create_router()
+        interface = self.routers_client.add_router_interface(
+            router['id'], subnet_id=subnet['id'])
+        self.assertIn('subnet_id', interface.keys())
+        self.assertIn('port_id', interface.keys())
+        # Verify router id is equal to device id in port details
+        show_port_body = self.ports_client.show_port(
+            interface['port_id'])
+        self.assertEqual(show_port_body['port']['device_id'],
+                         router['id'])
+        msg = "Subnet attached to a router interface must have a gateway IP"
+        kwargs = {'gateway_ip': None}
+        self.assertRaisesRegex(exceptions.BadRequest,
+                               msg,
+                               self.subnets_client.update_subnet,
+                               subnet['id'],
+                               **kwargs)
