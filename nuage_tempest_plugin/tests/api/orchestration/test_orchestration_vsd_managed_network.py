b1faf185fe7d34cbb60e2800f4baa551dfa62878
Tom Stappaerts <tom.stappaerts@nokia.com>
2019-08-20 10:34:38 +0200

    Add Heat test for project 2 netpartition mapping
    Change-Id: I2cb7a47ca8322bdf0912a5e6a386609c57777c2a
    Related-Bug: PROD-10454

--- nuage_tempest_plugin/tests/api/orchestration/test_orchestration_vsd_managed_network.py
+++ nuage_tempest_plugin/tests/api/orchestration/test_orchestration_vsd_managed_network.py
@@ -5,7 +5,7 @@ from netaddr import IPNetwork
 
 from tempest.lib.common.utils import data_utils
 
-from . import nuage_base
+from nuage_tempest_plugin.tests.api.orchestration import nuage_base
 
 from nuage_tempest_plugin.lib.test import nuage_test
 from nuage_tempest_plugin.lib.topology import Topology
@@ -264,6 +264,54 @@ class OrchestrationVsdManagedNetworkTest(
                          "Shall get the dhcp IP from the l2 domain")
         # in this case we get .2 correctly
         self.assertEqual(str(cidr[2]), subnet['allocation_pools'][0]['start'],
+                         "Shall start allocation pool at first address in "
+                         "l2 domain")
+        self.assertEqual(str(cidr[254]), subnet['allocation_pools'][0]['end'],
+                         "Shall end allocation pool at last address in "
+                         "l2 domain")
+
+    def test_link_subnet_to_vsd_l2domain_without_net_partition(self):
+        """test_link_subnet_to_vsd_l2domain_without_net_partition
+
+        Test heat creation should succeed for a private VSD managed
+        network without net-partition if nuagenet is in the default
+        net partition
+        """
+        # Create the VSD l2 domain from a template
+        name = data_utils.rand_name('l2domain-')
+        cidr = IPNetwork('10.10.100.0/24')
+
+        vsd_l2domain_template = self.create_vsd_dhcp_managed_l2domain_template(
+            name=name, cidr=cidr, gateway=str(cidr[1]))
+        vsd_l2domain = self.create_vsd_l2domain(
+            name=name, tid=vsd_l2domain_template[0]['ID'])
+
+        self.assertIsInstance(vsd_l2domain, list)
+        self.assertEqual(vsd_l2domain[0]['name'], name)
+        # launch a heat stack
+        stack_file_name = 'nuage_vsd_managed_network_no_netpartition'
+        stack_parameters = {
+            'vsd_subnet_id': vsd_l2domain[0]['ID'],
+            'private_net_name': self.private_net_name,
+            'private_net_cidr': str(cidr)}
+
+        self.launch_stack(stack_file_name, stack_parameters)
+        expected_resources = ['private_net', 'private_subnet']
+        self.verify_stack_resources(expected_resources,
+                                    self.template_resources,
+                                    self.test_resources)
+        network = self.verify_created_network('private_net')
+        subnet = self.verify_created_subnet('private_subnet', network)
+
+        self.assertTrue(subnet['enable_dhcp'],
+                        "Shall have DHCP enabled from the l2 domain template")
+        self.assertEqual(str(cidr), subnet['cidr'],
+                         "Shall get the CIDR from the l2 domain")
+        self.assertIsNone(subnet['gateway_ip'],
+                          "Shall not have gateway_ip as no dhcp option "
+                          "configured")
+        # in this case we get .2 correctly
+        self.assertEqual(str(cidr[1]), subnet['allocation_pools'][0]['start'],
                          "Shall start allocation pool at first address in "
                          "l2 domain")
         self.assertEqual(str(cidr[254]), subnet['allocation_pools'][0]['end'],
