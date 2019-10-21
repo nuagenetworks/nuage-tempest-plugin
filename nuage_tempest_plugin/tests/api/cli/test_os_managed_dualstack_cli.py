52e1b1e3deab77a543286bc5b07a1c95d1c5e960
Kris Sterckx <kris.sterckx@nuagenetworks.net>
2019-10-21 15:44:16 +0200

    Delete invalid VSD cidr validation tests
    With VSD-34209 now resolved, these tests are no longer applicable.
    
    Relates-To: VSD-34209
    Change-Id: Ice8313ce0f55a78e791199517fd25be9711086e1

--- nuage_tempest_plugin/tests/api/cli/test_os_managed_dualstack_cli.py
+++ nuage_tempest_plugin/tests/api/cli/test_os_managed_dualstack_cli.py
@@ -3,31 +3,16 @@
 
 from netaddr import IPNetwork
 
-from .base_nuage_networks_cli import BaseNuageNetworksCliTestCase
-from nuage_tempest_plugin.lib.features import NUAGE_FEATURES
-from nuage_tempest_plugin.lib.topology import Topology
-from nuage_tempest_plugin.services.nuage_network_client \
-    import NuageNetworkClientJSON
-
 from tempest.lib.common.utils import data_utils
 from tempest.lib import decorators
 
-LOG = Topology.get_logger(__name__)
+from nuage_tempest_plugin.services.nuage_network_client \
+    import NuageNetworkClientJSON
+from nuage_tempest_plugin.tests.api.cli.base_nuage_networks_cli \
+    import BaseNuageNetworksCliTestCase
 
-VALID_MAC_ADDRESS = 'fa:fa:3e:e8:e8:01'
-VALID_MAC_ADDRESS_2A = 'fa:fa:3e:e8:e8:2a'
-VALID_MAC_ADDRESS_2B = 'fa:fa:3e:e8:e8:2b'
 
-
-class OSManagedDualStackCliTest(
-        BaseNuageNetworksCliTestCase):
-
-    @classmethod
-    def skip_checks(cls):
-        super(OSManagedDualStackCliTest, cls).skip_checks()
-        if not NUAGE_FEATURES.os_managed_dualstack_subnets:
-            raise cls.skipException(
-                'OS Managed Dual Stack is not supported in this release')
+class OSManagedDualStackCliTest(BaseNuageNetworksCliTestCase):
 
     @classmethod
     def setup_clients(cls):
@@ -135,53 +120,3 @@ class OSManagedDualStackCliTest(
                                  .format(subnet6['id']),
                                  self.show_subnet,
                                  subnet6['id'])
-
-    @decorators.attr(type='smoke')
-    def test_dualstack_with_reserved_ipv6_address_ipv4_first_neg(self):
-        network_name = data_utils.rand_name('cli_network')
-        network = self.create_network_with_args(network_name)
-        self.addCleanup(self._delete_network, network['id'])
-        self.networks.remove(network)
-
-        subnet_name = data_utils.rand_name('cli-subnet')
-
-        cidr4 = IPNetwork('1.1.20.0/24')
-        cidr6 = IPNetwork("2002:5f74:c4a5:b82e::/64")
-
-        subnet4 = self.create_subnet_with_args(
-            network['name'], str(cidr4),
-            "--name ", subnet_name + "-4")
-        self.subnets.remove(subnet4)
-        self.addCleanup(self._delete_subnet, subnet4['id'])
-        show_subnet4 = self.show_subnet(subnet4['id'])
-        self.assertEqual(subnet4['id'],
-                         show_subnet4['id'], "subnet not found")
-
-        self.assertCommandFailed("IP Address {} is not valid"
-                                 " or cannot be in reserved address space."
-                                 .format(str(cidr6)),
-                                 self.create_subnet_with_args,
-                                 network['name'], str(cidr6),
-                                 "--name ", subnet_name + "-6",
-                                 "--ip-version 6",
-                                 "--disable-dhcp ")
-
-    @decorators.attr(type='smoke')
-    def test_dualstack_with_reserved_ipv6_address_neg(self):
-        network_name = data_utils.rand_name('cli_network')
-        network = self.create_network_with_args(network_name)
-        self.addCleanup(self._delete_network, network['id'])
-        self.networks.remove(network)
-
-        subnet_name = data_utils.rand_name('cli-subnet')
-
-        cidr6 = IPNetwork("2002:5f74:c4a5:b82e::/64")
-
-        self.assertCommandFailed("IP Address {} is not valid"
-                                 " or cannot be in reserved address space."
-                                 .format(str(cidr6)),
-                                 self.create_subnet_with_args,
-                                 network['name'],
-                                 str(cidr6),
-                                 "--name ", subnet_name + "-6",
-                                 "--ip-version 6")
