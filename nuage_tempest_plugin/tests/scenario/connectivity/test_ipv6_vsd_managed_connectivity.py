423b6f09656ddcd7be6080860a491f461c5ec145
Vlad Gridin <vladyslav.gridin@nokia.com>
2020-01-14 12:55:04 +0100

    Mark vsd managed tcp connectivity tests unstable
    Introduce a new decorator - unstable_test(), to allow
    to run tests marked as unstable without failing gate.
    Mark following tests as unstable as they fail often on ospd
    deployments:
      - test_tcp_connectivity_stateful_acl_l3_vsd_managed
      - test_tcp_connectivity_stateless_acl_l3_vsd_managed
      - test_tcp_connectivity_stateful_acl_l3_vsd_managed_ipv6
    
    Change-Id: Ifcff6b9e0a1d755d29e18b74ee8356616e96b0f7

--- nuage_tempest_plugin/tests/scenario/connectivity/test_ipv6_vsd_managed_connectivity.py
+++ nuage_tempest_plugin/tests/scenario/connectivity/test_ipv6_vsd_managed_connectivity.py
@@ -8,6 +8,7 @@ import testtools
 from tempest.lib import decorators
 
 from nuage_tempest_plugin.lib.test.nuage_test import NuageBaseTest
+from nuage_tempest_plugin.lib.test.nuage_test import unstable_test
 from nuage_tempest_plugin.lib.topology import Topology
 
 CONF = Topology.get_conf()
@@ -496,6 +497,7 @@ class Ipv6L3VsdManagedConnectivityTest(NuageBaseTest):
     @testtools.skipUnless(Topology.up_to_openstack('train'),
                           'Upstream bug in Ussuri: '
                           'https://bugs.launchpad.net/neutron/+bug/1859163')
+    @unstable_test(bug='OPENSTACK-2782')
     def test_tcp_connectivity_stateful_acl_l3_vsd_managed_ipv6(self):
         network, vsd_l3domain = self._create_vsd_managed_resources()
         ingress_tpl, egress_tpl = self.vsd.create_acl_templates(vsd_l3domain)
