7835e812d7ab7309ffba0e95bca844969bdd69cb
Tom Stappaerts <tom.stappaerts@nokia.com>
2020-06-03 18:27:48 +0200

    Test A/A and A/S HW VTEP
    Change-Id: Ic1666bcb08c972168263fe770973fb56863f80aa
    Partial-Bug: PROD-9694

--- nuage_tempest_plugin/tests/api/test_nuage_hwvtep.py
+++ nuage_tempest_plugin/tests/api/test_nuage_hwvtep.py
@@ -22,12 +22,16 @@ from tempest.lib import decorators
 
 from nuage_tempest_plugin.lib.test.nuage_test import NuageBaseTest
 from nuage_tempest_plugin.lib.topology import Topology
+from nuage_tempest_plugin.lib.utils import constants
+
 
 CONF = Topology.get_conf()
 LOG = Topology.get_logger(__name__)
 
 
 class TestNuageHWVTEP(NuageBaseTest):
+
+    physnet = 'physnet1'
 
     @classmethod
     def skip_checks(cls):
@@ -40,16 +44,16 @@ class TestNuageHWVTEP(NuageBaseTest):
     def _test_hw_vtep(self, is_l3=False, is_vsd_managed=False, is_ipv4=True,
                       is_ipv6=False, is_flat=False):
         (ipv4_subnet, ipv6_subnet,
-         network, vsd_resource) = self._create_network_subnets(
+         network, vsd_resource, vsd_domain) = self._create_network_subnets(
             is_ipv4, is_ipv6, is_l3, is_vsd_managed, is_flat)
         if not is_vsd_managed:
             if is_ipv4:
-                vsd_resource = self.vsd.get_l2domain(
+                vsd_resource = vsd_domain = self.vsd.get_l2domain(
                     by_network_id=network['id'],
                     cidr=ipv4_subnet['cidr'],
                     ip_type=4)
             else:
-                vsd_resource = self.vsd.get_l2domain(
+                vsd_resource = vsd_domain = self.vsd.get_l2domain(
                     by_network_id=network['id'],
                     cidr=ipv6_subnet['cidr'],
                     ip_type=6)
@@ -64,19 +68,31 @@ class TestNuageHWVTEP(NuageBaseTest):
         # Create VM
         vm = self.create_tenant_server(networks=[network],
                                        prepare_for_connectivity=False)
-        # Get BRIDGE vport
-        self._verify_bridge_port(network, vsd_resource, is_flat)
-        # Get Bridge interface
+        if is_vsd_managed:
+            pg = self._get_pg_allow_all(vsd_domain,
+                                        should_have_pg=False)
+            pg_id = None
+        else:
+            pg = self._get_pg_allow_all(vsd_domain)
+            pg_id = pg.id
+            self._verify_pg_allow_all(pg)
+
+        self._verify_bridge_port(network, vsd_resource, is_flat,
+                                 expected_pg_id=pg_id)
+
+        # Create second VM, no change expected
         vm2 = self.create_tenant_server(networks=[network],
                                         prepare_for_connectivity=False)
 
-        self._verify_bridge_port(network, vsd_resource, is_flat)
+        self._verify_bridge_port(network, vsd_resource, is_flat,
+                                 expected_pg_id=pg_id)
 
         self.manager.servers_client.delete_server(vm.id)
         waiters.wait_for_server_termination(
             self.manager.servers_client, vm.id)
 
-        self._verify_bridge_port(network, vsd_resource, is_flat)
+        self._verify_bridge_port(network, vsd_resource, is_flat,
+                                 expected_pg_id=pg_id)
 
         self.manager.servers_client.delete_server(vm2.id)
         waiters.wait_for_server_termination(
@@ -92,21 +108,22 @@ class TestNuageHWVTEP(NuageBaseTest):
         project_id = network['project_id']
         if is_flat:
             kwargs = {'provider:network_type': 'flat',
-                      'provider:physical_network': 'physnet1'}
+                      'provider:physical_network': self.physnet}
         else:
-            kwargs = {}
+            kwargs = {'provider:network_type': 'vlan',
+                      'provider:physical_network': self.physnet}
         network = self.create_network(manager=self.admin_manager,
                                       project_id=project_id, **kwargs)
         network = self.get_network(network['id'], manager=self.admin_manager)
-        ipv4_subnet = ipv6_subnet = vsd_resource = None
+        ipv4_subnet = ipv6_subnet = vsd_resource = vsd_domain = None
         if is_vsd_managed:
             # create vsd managed resources
             if is_l3:
                 vsd_l3domain_template = self.vsd_create_l3domain_template()
-                vsd_l3domain = self.vsd_create_l3domain(
+                vsd_domain = self.vsd_create_l3domain(
                     template_id=vsd_l3domain_template.id)
 
-                vsd_zone = self.vsd_create_zone(domain=vsd_l3domain)
+                vsd_zone = self.vsd_create_zone(domain=vsd_domain)
                 subnet_cidr = IPNetwork('10.10.100.0/24')
                 subnet_gateway = str(IPAddress(subnet_cidr) + 1)
                 subnet_ipv6_cidr = IPNetwork("2001:5f74:c4a5:b82e::/64")
@@ -158,7 +175,7 @@ class TestNuageHWVTEP(NuageBaseTest):
                         dhcp_managed=True,
                         cidr4=self.cidr4,
                         cidr6=self.cidr6)
-                vsd_resource = self.vsd_create_l2domain(
+                vsd_resource = vsd_domain = self.vsd_create_l2domain(
                     template=vsd_l2domain_template)
                 if is_ipv4:
                     ipv4_subnet = self.create_subnet(
@@ -186,20 +203,59 @@ class TestNuageHWVTEP(NuageBaseTest):
                 ipv4_subnet = self.create_subnet(network)
             if is_ipv6:
                 ipv6_subnet = self.create_subnet(network, ip_version=6)
-        return ipv4_subnet, ipv6_subnet, network, vsd_resource
+        return ipv4_subnet, ipv6_subnet, network, vsd_resource, vsd_domain
 
-    def _verify_bridge_port(self, network, vsd_resource, is_flat):
+    def _get_pg_allow_all(self, parent_resource, should_have_pg=True):
+        pgs = parent_resource.policy_groups.get(
+            filter=self.vsd.get_external_id_filter('hw:PG_ALLOW_ALL'))
+        if should_have_pg:
+            self.assertEqual(1, len(pgs), 'Exactly 1 PG_ALLOW_ALL_HW expected')
+            return pgs[0]
+        else:
+            self.assertEmpty(pgs)
+            return None
+
+    def _verify_pg_allow_all(self, policy_group):
+        # Verify PG
+        self.assertEqual(policy_group.type, 'HARDWARE')
+        self.assertEqual(policy_group.name,
+                         constants.NUAGE_PLCY_GRP_ALLOW_ALL_HW)
+        self.assertEqual(policy_group.description,
+                         constants.NUAGE_PLCY_GRP_ALLOW_ALL_HW)
+
+        # Verify associated ACL
+        in_rule = self.vsd.get_ingress_acl_entry(
+            vspk_filter='locationID == "{}"'.format(policy_group.id))
+        eg_rule = self.vsd.get_egress_acl_entry(
+            vspk_filter='locationID == "{}"'.format(policy_group.id))
+
+        self.assertIsNotNone(in_rule, "in_rule must not be None")
+        self.assertEqual(in_rule.network_type, 'ANY')
+        self.assertEqual(in_rule.location_type, 'POLICYGROUP')
+
+        self.assertIsNotNone(eg_rule, "eg_rule must not be None")
+        self.assertEqual(eg_rule.network_type, 'ANY')
+        self.assertEqual(eg_rule.location_type, 'POLICYGROUP')
+
+    def _verify_bridge_port(self, network, vsd_resource, is_flat,
+                            expected_pg_id):
         vports = vsd_resource.vports.get()
         self.assertEqual(1, len(vports), 'Exactly one vport expected')
         vport = vports[0]
         self.assertEqual('BRIDGE', vport.type)
-        if not is_flat:
-            vlan = network['provider:segmentation_id']
-        else:
+        if is_flat:
             vlan = 0
+        else:
+            vlan = network['provider:segmentation_id']
         self.assertEqual(vlan, vport.vlan)
         interfaces = vport.bridge_interfaces.get()
         self.assertEqual(1, len(interfaces))
+        policy_groups = vport.policy_groups.get()
+        if expected_pg_id:
+            self.assertEqual(1, len(policy_groups))
+            self.assertEqual(expected_pg_id, policy_groups[0].id)
+        else:
+            self.assertEmpty(policy_groups)
 
     @decorators.attr(type='smoke')
     def test_nuage_hwvtep_os_managed_ipv4_with_vm(self):
@@ -284,3 +340,64 @@ class TestNuageHWVTEP(NuageBaseTest):
     def test_nuage_hwvtep_vsd_managed_flat_l3_dualstack_with_vm(self):
         self._test_hw_vtep(is_vsd_managed=True, is_ipv4=True, is_ipv6=True,
                            is_l3=True, is_flat=True)
+
+
+class TestNuageHWVTEPActiveActive(TestNuageHWVTEP):
+
+    physnet = 'physnet2'
+
+    def _verify_bridge_port(self, network, vsd_resource, is_flat,
+                            expected_pg_id):
+        """_verify_bridge_port
+
+        Physnet2 is attached to br-active
+        br-active is connected to two GW configured as LAG with a RG.
+        """
+        vports = vsd_resource.vports.get()
+        self.assertEqual(1, len(vports), 'Exactly one vport expected')
+        vport = vports[0]
+        self.assertEqual('BRIDGE', vport.type)
+        if is_flat:
+            vlan = 0
+        else:
+            vlan = network['provider:segmentation_id']
+        self.assertEqual(vlan, vport.vlan)
+        self.assertEqual('REDUNDANT_GW_GRP', vport.associated_gateway_type)
+        interfaces = vport.bridge_interfaces.get()
+        self.assertEqual(1, len(interfaces))
+        policy_groups = vport.policy_groups.get()
+        if expected_pg_id:
+            self.assertEqual(1, len(policy_groups))
+            self.assertEqual(expected_pg_id, policy_groups[0].id)
+        else:
+            self.assertEmpty(policy_groups)
+
+
+class TestNuageHWVTEPActiveStandby(TestNuageHWVTEP):
+
+    physnet = 'physnet3'
+
+    def _verify_bridge_port(self, network, vsd_resource, is_flat,
+                            expected_pg_id):
+        """_verify_bridge_port
+
+        Physnet3 is attached to br-standby
+        br-standby is connected to two GW, not configured as LAG.
+        """
+        vports = vsd_resource.vports.get()
+        self.assertEqual(2, len(vports), 'Exactly two vports expected')
+        for vport in vports:
+            self.assertEqual('BRIDGE', vport.type)
+            if is_flat:
+                vlan = 0
+            else:
+                vlan = network['provider:segmentation_id']
+            self.assertEqual(vlan, vport.vlan)
+            interfaces = vport.bridge_interfaces.get()
+            self.assertEqual(1, len(interfaces))
+            policy_groups = vport.policy_groups.get()
+            if expected_pg_id:
+                self.assertEqual(1, len(policy_groups))
+                self.assertEqual(expected_pg_id, policy_groups[0].id)
+            else:
+                self.assertEmpty(policy_groups)
